b93d277323c563e80391e97a271442f9fcf4caea
Heloisa Godoy <godoymariaheloisa@gmail.com>
2025-08-20 08:34:10 -0300

    Autenticating with Firebase

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,20 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Pegue isso no Console do Firebase (Configurações do projeto)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:xxxxxxxxxxxxxx"
+  apiKey: "AIzaSyArX5MuCavqggXgYkRGmfBpUjh8P6pVxMQ",
+  authDomain: "barkbuddy-bd.firebaseapp.com",
+  projectId: "barkbuddy-bd",
+  storageBucket: "barkbuddy-bd.firebasestorage.app",
+  messagingSenderId: "41034629472",
+  appId: "1:41034629472:web:5b975afff21197bedb5e05",
+  measurementId: "G-PXFPK5M1N4"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const analytics = getAnalytics(app);
