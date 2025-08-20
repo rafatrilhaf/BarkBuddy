bffecd5bb260e0ba27f67555168148fdd7de6634
João Pedro <165198179+joaojotarc@users.noreply.github.com>
2025-08-20 09:05:51 -0300

    Login e Cadastro Funcionando.

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// services/firebase.ts
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyArX5MuCavqggXgYkRGmfBpUjh8P6pVxMQ",
   authDomain: "barkbuddy-bd.firebaseapp.com",
@@ -13,9 +10,9 @@ const firebaseConfig = {
   storageBucket: "barkbuddy-bd.firebasestorage.app",
   messagingSenderId: "41034629472",
   appId: "1:41034629472:web:5b975afff21197bedb5e05",
-  measurementId: "G-PXFPK5M1N4"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
